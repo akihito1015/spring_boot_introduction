835ba74bf224ccbf51b9aceb2cebda12c88c9abe
Takamura Takumi <koutarou0827@icloud.com>
2023-09-15 15:52:27 +0900

    Lesson08実装完了

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Employee;
 import com.example.repository.EmployeeRepository;
 
 @Service
@@ -16,4 +20,22 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
     // ビジネスロジック用のメソッドを以降に定義します
+    public List<Employee> findAllEmployee() {
+        // employeesテーブルのデータをemployeeオブジェクトにつめて全件取得する
+        return this.employeeRepository.findAll();
+    }
+    
+    // 主キーでの1件検索
+    public Employee findEmployee(Integer employeeId) {
+        // データの1件取得
+        Optional<Employee> optionalEmployee = this.employeeRepository.findById(employeeId);
+        // OptionalからEntityクラスの取得を試みる
+        Employee employee = optionalEmployee.get();
+        return employee;
+    }
+    
+    // 絞り込み検索
+    public List<Employee> findByName(String name) {
+        return this.employeeRepository.findByName(name);
+    }
 }
