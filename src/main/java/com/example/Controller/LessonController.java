ddcbd1b61237573d63849c8ab318a251f9e64f4b
Takamura Takumi <koutarou0827@icloud.com>
2023-09-15 10:50:41 +0900

    Lesson04実装完了

--- src/main/java/com/example/Controller/LessonController.java
+++ src/main/java/com/example/Controller/LessonController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/lesson")
@@ -17,5 +19,15 @@ public class LessonController {
         // messageという属性名、Hello Spring Boot!!という値を追加します
         model.addAttribute("message", text);
         return "index";
+    }
+    
+    @GetMapping("/request_test")
+    // 返り値をレスポンスとして扱います
+    @ResponseBody
+    // name ... 名前、 bloodType ... 血液型
+    // パラメータ名と仮引数名が同じな場合、@RequestParam String nameのようにパラメータ名の指定を省略できます
+    public String getTest(@RequestParam("name") String name
+                          , @RequestParam("bloodType") String bloodType) {
+        return "名前: " + name + "<br>血液型: " + bloodType;
     }
 }
