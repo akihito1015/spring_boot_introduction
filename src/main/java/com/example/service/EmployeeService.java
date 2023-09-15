7cf8442909cf25e43d202a44dfac08e73729a1ea
Takamura Takumi <koutarou0827@icloud.com>
2023-09-15 16:06:01 +0900

    Lesson09実装完了

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -38,4 +38,36 @@ public class EmployeeService {
     public List<Employee> findByName(String name) {
         return this.employeeRepository.findByName(name);
     }
+    
+    // 新規登録処理
+    public Employee insert(String name, String department) {
+        // 保存したいEntityクラスのインスタンスを作成する
+        Employee employee = new Employee();
+
+        // 引数で受けたname, departmentをEmployeeオブジェクトにセットします
+        employee.setName(name);
+        employee.setDepartment(department);
+
+        // データベースに保存する
+        return this.employeeRepository.save(employee);
+    }
+    
+    // 更新処理
+    public Employee update(Integer employeeId, String name, String department) {
+        // 更新したいデータを取得する
+        Optional<Employee> optionalEmployee = this.employeeRepository.findById(employeeId);
+        Employee employee = optionalEmployee.get();
+
+        // Entityクラスのフィールドに更新内容をセットする
+        employee.setName(name);
+        employee.setDepartment(department);
+
+        // データベースに保存する
+        return this.employeeRepository.save(employee);
+    }
+    
+    // 削除処理
+    public void delete(Integer id) {
+        this.employeeRepository.deleteById(id);
+    }
 }
